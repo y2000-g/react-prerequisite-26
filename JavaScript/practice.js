b22e54ac50ba1986da1bc1cada75fe05f91d7e7a
Yashita Gole <yashitagole2019@gmail.com>
2026-04-30 20:03:01 +0530

    updating article page project with css and adding js practice file

---
+++ JavaScript/practice.js
@@ -0,0 +1,19 @@
+// Data type - Number
+// All JavaScript numbers are stored as decimal numbers (floating point)
+// Numbers can be written with, or without decimals:
+
+let x1 = 45.00;
+let x2 = 34; 
+
+console.log("x1", x1)
+console.log("x2", x2)
+console.log(typeof(x1))
+console.log(typeof(x2))
+
+// Data type - A string (a text string) is a series of characters
+let para1 = "This is 'Cat'"
+let para2 = 'and this is "Dog"'
+let para3 = " - Both are Domestic animals"
+
+console.log("Answer:", para1 + para2 + para3 )
+console.log(typeof(para1))
