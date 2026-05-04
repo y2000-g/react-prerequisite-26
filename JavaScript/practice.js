c5c1e1a4b1e64404ae9762c05196349a61bf16b2
Yashita Gole <yashitagole2019@gmail.com>
2026-05-04 18:05:18 +0530

    "Adding logical Practice files"

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -7,7 +7,7 @@ let x2 = 34;
 
 console.log("x1", x1)
 console.log("x2", x2)
-console.log(typeof(x1))
+console.log(typeof x1)
 console.log(typeof(x2))
 
 // Data type - A string (a text string) is a series of characters
@@ -17,3 +17,129 @@ let para3 = " - Both are Domestic animals"
 
 console.log("Answer:", para1 + para2 + para3 )
 console.log(typeof(para1))
+
+// BigInt
+let num = 1234567890123456789012345n;
+let numw2 = BigInt(1234567890123456789012345)
+console.log("num", num)
+console.log(typeof(num))
+console.log("num", numw2)
+console.log(typeof(numw2))
+
+
+// Boolean
+let x = true;
+let y = false;
+x == 8
+console.log("boolean x", x == 9)
+console.log(typeof(x))
+
+// Object
+const person = {firstName:"John", lastName:"Doe"};
+console.log("Object", person)
+console.log(typeof(person))
+
+// Undefined
+let h = " ";
+let i;
+
+// Null
+let c = null;
+let d = null;
+
+// Symbol
+// const k = new Symbol();
+// const l = Symbol();
+// console.log("symbol", k)
+
+// Scope 
+var num1 = 10
+let num2 = 20
+{
+    console.log(num1)
+    console.log(num2)
+    var num3 = 30
+    let num4 = 40
+    console.log(num3)
+    console.log(num4)
+    {
+        console.log(num2)
+        console.log(num3)
+        console.log(num4)
+    }
+}
+console.log(num1)
+console.log(num2)
+console.log(num3)
+// console.log(num4)
+
+let hexaVar = 0x123
+let octalVar = 0o1234
+let binaryVar = 0b010100
+
+console.log(hexaVar)
+console.log(octalVar)
+console.log(binaryVar)
+
+let exp = 9000
+let expNum = 9e3
+let exp1 = 0.00123
+let exp1Num = 123e-5
+let a = 1/0
+console.log(a)
+console.log(expNum)
+console.log(exp1Num)
+console.log(typeof a)
+let b = -Infinity
+console.log(b)
+console.log(typeof b)
+
+let strSingleQuote = 'Yashita "Gole"'
+console.log(strSingleQuote)
+let string = "abc\\123"
+console.log(string)
+let string2 = "Front-end \"class\""
+console.log(string2)
+let test = "100"*"10"
+console.log("Test", test)
+
+// Boolean
+let state = 5
+console.log("Result:", state == 8)
+console.log("Result:", state != 8)
+
+// Number Methods
+let numMethod = 10000
+num.toString();
+console.log("numbertoString", numMethod)
+console.log(typeof numMethod)
+
+numMethod.toPrecision(3);
+console.log("numbertoPrecision", numMethod)
+
+let numVar = 123.456
+console.log(numVar.toPrecision(2))
+console.log(numVar.toPrecision(4))
+console.log(numVar.toPrecision(6))
+console.log(numVar.toPrecision(8))
+
+let str3 = "Hello"
+str3.concat(" ", "World", " ") 
+
+let bigInt1 = 123456789000234000045677n
+let bigInt2 = 1n 
+console.log(typeof bigInt1)
+console.log(typeof bigInt2)
+
+// let bigInt3 = 1000n/0n 
+
+// String Interpolation
+let myName = "Yashita"
+console.log("Hey My name is "+myName)
+console.log(`Hey My name is ${myName}`)
+
+console.time()
+console.log("Test console ")
+console.timeEnd()
+// myfunction()
+// str3.myfuntion()
--- Logical questions/logicalPractice.js
+++ Logical questions/logicalPractice.js
@@ -42,13 +42,125 @@ for( fac = 1; fac <= givenNum; fac++)
 }
 console.log(result)
 
-// Program - 4 (Astric)
-let ast = "*"
+// Program - 4 (Whole Square  Astric)
+
 for(let sqr = 1; sqr <= 5; sqr++)
 {
+    let ast = ""
     for( let sqrInner = 1; sqrInner <= 5; sqrInner++)
     {
-        ast = ast + "*"
+        ast = ast + "* "
     }
+    console.log(ast)
 }
-console.log(ast)
+console.log("\n")
+
+// Program - 5 (hollow Square Astric)
+let n = 5
+for(let sqr1 = 1; sqr1 <= n; sqr1++)
+{
+    let ast1 = ""
+    for( let sqrInner1 = 1; sqrInner1 <= n; sqrInner1++)
+    {
+        if(sqr1 === 1 || sqr1 === n || sqrInner1 === 1 || sqrInner1 === n)
+        {
+           ast1 = ast1 + "* "
+        }
+        else{
+            ast1 = ast1 + "  "
+        }
+        
+    }
+    console.log(ast1)
+}
+console.log("\n")
+
+// Program - 4 (WHole Square hash)
+
+for(let sqr2 = 1; sqr2 <= 5; sqr2++)
+{
+    let ast2 = ""
+    for( let sqrInner2 = 1; sqrInner2 <= 5; sqrInner2++)
+    {
+        ast2 = ast2 + "# "
+    } 
+    console.log(ast2)
+}
+console.log("\n")
+
+// Program - 5 (Hollow Square hash)
+let n2 = 5
+for(let sqr3 = 1; sqr3 <= n2; sqr3++)
+{
+    let ast3 = ""
+    for( let sqrInner3 = 1; sqrInner3 <= n; sqrInner3++)
+    {
+        if(sqr3 === 1 || sqr3 === n2 || sqrInner3 === 1 || sqrInner3 === n2)
+        {
+           ast3 = ast3 + "# "
+        }
+        else{
+            ast3 = ast3 + "  "
+        }
+        
+    }
+    console.log(ast3)
+}
+console.log("\n")
+
+// Make a Hollow square with sides coming from a variable NxN
+
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// readline.question("Enter number:", (num) => {
+//     console.log("Number is " + num);
+//     num = Number(num); 
+
+//     for(let sqrOuter = 1; sqrOuter <= num; sqrOuter++)
+//     {
+//         let SqrSide = ""
+//         for( let sqrInnerindex = 1; sqrInnerindex <= num; sqrInnerindex++)
+//         {
+//             if(sqrOuter === 1 || sqrOuter === num || sqrInnerindex === 1 || sqrInnerindex === num)
+//             {
+//             SqrSide = SqrSide + "* "
+//             }
+//             else{
+//                 SqrSide = SqrSide + "  "
+//             }
+            
+//         }
+//         console.log(SqrSide)
+//     }
+
+//     console.log("\n");
+//     readline.close();
+// });
+
+// Make a square with sides coming from a variable NxN
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question("Enter number:", (num) => {
+    console.log("Number is " + num);
+    num = Number(num); 
+
+    for(let sqrOuter = 1; sqrOuter <= num; sqrOuter++)
+    {
+        let SqrSide = ""
+        for( let sqrInnerindex = 1; sqrInnerindex <= num; sqrInnerindex++)
+        {
+            SqrSide = SqrSide + "* "            
+        }
+        console.log(SqrSide)
+    }
+
+    console.log("\n");
+    readline.close();
+});
